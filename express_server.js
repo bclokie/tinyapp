7293844949673b3cbb52a53755d3f384e36853e8
Braden <braden.clokie@gmail.com>
2023-01-23 20:40:04 -0500

    Added function to show single URL

--- express_server.js
+++ express_server.js
@@ -33,3 +33,13 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
