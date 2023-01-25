a7db620d234902e28eac4ab2248899902c0ce704
Braden <braden.clokie@gmail.com>
2023-01-25 14:28:52 -0500

    Added display username functionality

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,21 +37,30 @@ app.get("/hello", (req, res) => {
 });
 
  app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies,
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies,
+  };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`urls/${shortURL}`);
@@ -60,7 +71,18 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+//Login feature
+
+app.post("/login", (req, res) => {
+  console.log(req.body.username);
+  const username = req.body.username;
+
+  res.cookie('name', username);
+  res.redirect('/urls');
+});
+
 //Will log once the server starts
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
