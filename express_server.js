6919031cd77cce161c72a7cd7cbda93febd06f4b
Braden <braden.clokie@gmail.com>
2023-01-24 21:38:59 -0500

    Had a weird issue so I tinkered with a lot of stuff and now I forget what I changed so we're pushing it all

--- express_server.js
+++ express_server.js
@@ -5,6 +5,11 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 const generateRandomString = () => {
   let string = '';
   const cipher = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -14,13 +19,6 @@ const generateRandomString = () => {
     i++;
   }
   return string;
-};
-
-console.log(generateRandomString());
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -35,15 +33,6 @@ app.post("/urls/:id/delete", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
  app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -69,4 +58,9 @@ app.post("/urls", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
+});
+
+//Will log once the server starts
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
